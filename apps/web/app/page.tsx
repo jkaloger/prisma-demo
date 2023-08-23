47e73e1f8daf8770602ad5fa389bcd39b9627019
Jack Kaloger <jack.kaloger@inlight.com.au>
2023-08-23 14:18:08 +1000

    mysql prisma

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,17 @@
-export default function Page(): JSX.Element {
+import { PrismaClient } from "db";
+
+const client = new PrismaClient();
+
+export default async function Page() {
+  const users = await client.user.findMany();
+
   return (
     <>
       <div>Hello World!</div>
       <div>This is a Page</div>
+      {users.map((user) => (
+        <div key={user.id}>{user.name}</div>
+      ))}
     </>
   );
 }
